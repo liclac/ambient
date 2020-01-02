f1a129ab2a4a8d1acc6eadb2d635dbb753ac30c7
embr <git@liclac.eu>
2020-01-02 19:53:56 +0000

    Cache SSID

--- functions.d/ambient_get_ssid.fish
+++ functions.d/ambient_get_ssid.fish
@@ -1,3 +1,6 @@
 function ambient_get_ssid
-    iw dev | grep ssid | awk '{print $2}'
+    if not set -q ambient_ssid
+        set -g ambient_ssid (iw dev | grep ssid | awk '{print $2}')
+    end
+    echo $ambient_ssid
 end
