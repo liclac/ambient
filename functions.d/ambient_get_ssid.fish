5ebabcbce87b0ac2d7aa456028d67a92c1da535c
Tjark Saul <mail@tjarksaul.de>
2020-01-08 22:19:09 +0100

    Update macOS support

--- functions.d/ambient_get_ssid.fish
+++ functions.d/ambient_get_ssid.fish
@@ -1,11 +1,10 @@
 function ambient_get_ssid
     if not set -q ambient_ssid
-    	set os (uname)
-    	if [ $os = "Darwin" ]
-        	set -g ambient_ssid (/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F: '/ SSID/{print $2}')
-    	else 
-    		set -g ambient_ssid (iw dev | grep ssid | awk '{print $2}')
-		end
+        if test (uname) = "Darwin"
+            set -g ambient_ssid (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | sed -n 's/^ *SSID: //p')
+        else
+            set -g ambient_ssid (iw dev | grep ssid | awk '{print $2}')
+        end
     end
     echo $ambient_ssid
 end
