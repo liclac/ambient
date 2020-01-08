8bb588ebaa8109ae63263ee2386d06bc9096c39a
Tjark Saul <mail@tjarksaul.de>
2020-01-08 17:49:21 +0100

    Widget icons macOS comptabile

--- lib/_dispatch_widgets.fish
+++ lib/_dispatch_widgets.fish
@@ -1,4 +1,11 @@
 #!/usr/bin/env fish
+set os (uname)
+if [ $os = "Darwin" ]
+	set -g icons 🚏 ⏱ 🚄 💨
+else 
+	set -g icons    
+end
+
 for file in ./widgets.d/**/*.fish
     source $file
 end
--- widgets.d/trains/EU_Eurostar.fish
+++ widgets.d/trains/EU_Eurostar.fish
@@ -1,6 +1,6 @@
 if set -q AMBIENT_EU_EUROSTAR_TRAIN_ID
-    echo " Eurostar $AMBIENT_EU_EUROSTAR_TRAIN_ID"
+    echo "$icons[3] Eurostar $AMBIENT_EU_EUROSTAR_TRAIN_ID"
 end
 if set -q AMBIENT_EU_EUROSTAR_SPEED
-    echo " $AMBIENT_EU_EUROSTAR_SPEED km/h"
+    echo "$icons[4] $AMBIENT_EU_EUROSTAR_SPEED km/h"
 end
