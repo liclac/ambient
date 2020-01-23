97159ab6b599859711021cf5d3a795c9ce7eab16
Tjark Saul <mail@tjarksaul.de>
2020-01-23 19:23:46 +0100

    Always use Emoji for widgets

--- lib/_dispatch_widgets.fish
+++ lib/_dispatch_widgets.fish
@@ -1,11 +1,5 @@
 #!/usr/bin/env fish
 
-if test (uname) = "Darwin"
-	set -g icons 🚏 ⏱ 🚄 💨
-else 
-	set -g icons    
-end
-
 for file in ./widgets.d/**/*.fish
     source $file
 end
--- widgets.d/trains/EU_Eurostar.fish
+++ widgets.d/trains/EU_Eurostar.fish
@@ -1,6 +1,6 @@
 if set -q AMBIENT_EU_EUROSTAR_TRAIN_ID
-    echo "$icons[3] Eurostar $AMBIENT_EU_EUROSTAR_TRAIN_ID"
+    echo "🚄 Eurostar $AMBIENT_EU_EUROSTAR_TRAIN_ID"
 end
 if set -q AMBIENT_EU_EUROSTAR_SPEED
-    echo "$icons[4] $AMBIENT_EU_EUROSTAR_SPEED km/h"
+    echo "💨 $AMBIENT_EU_EUROSTAR_SPEED km/h"
 end
