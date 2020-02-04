9ad960a229a328bc241e73bc1d6f360431a764f8
Dominik M. Kwiatek <dom@dom.dog>
2020-02-04 08:17:28 +0000

    Add Thalys

--- ambient.d/trains/_icomera.fish
+++ ambient.d/trains/_icomera.fish
@@ -1,32 +1,35 @@
 set -l ssid (ambient_get_ssid)
 switch $ssid
 case "GatwickExpress_WiFi"
-  set -g ambient_icomera_country GB
-  set -g ambient_icomera_provider "Gatwick Express"
+  set ambient_icomera_country GB
+  set ambient_icomera_provider "Gatwick Express"
 case "Thameslink_WiFi"
-  set -g ambient_icomera_country GB
-  set -g ambient_icomera_provider Thameslink
+  set ambient_icomera_country GB
+  set ambient_icomera_provider Thameslink
 case "Southern_WiFi"
-  set -g ambient_icomera_country GB
-  set -g ambient_icomera_provider Southern
+  set ambient_icomera_country GB
+  set ambient_icomera_provider Southern
 case "GreatNorthern_WiFi"
-  set -g ambient_icomera_country GB
-  set -g ambient_icomera_provider "Great Northern"
+  set ambient_icomera_country GB
+  set ambient_icomera_provider "Great Northern"
 case "SWR WiFi"
-  set -g ambient_icomera_country GB
-  set -g ambient_icomera_provider "South Western Railway"
+  set ambient_icomera_country GB
+  set ambient_icomera_provider "South Western Railway"
 case "VirginTrainsEC-WiFi"
-  set -g ambient_icomera_country GB
-  set -g ambient_icomera_provider "Virgin Trains East Coast"
+  set ambient_icomera_country GB
+  set ambient_icomera_provider "Virgin Trains East Coast"
 case "SJ"
-  set -g ambient_icomera_country SE
-  set -g ambient_icomera_provider SJ
+  set ambient_icomera_country SE
+  set ambient_icomera_provider SJ
 case "Irish Rail - WiFi"
-  set -g ambient_icomera_country IE
-  set -g ambient_icomera_provider "Irish Rail"
+  set ambient_icomera_country IE
+  set ambient_icomera_provider "Irish Rail"
 case "WIFIonICE"
-  set -g ambient_icomera_country DE
-  set -g ambient_icomera_provider "Deutsche Bahn"
+  set ambient_icomera_country DE
+  set ambient_icomera_provider "Deutsche Bahn"
+case "THALYSNET"
+  set ambient_icomera_country FR
+  set ambient_icomera_country Thalys
 end
 
 if test -n $ambient_icomera_provider
@@ -38,7 +41,7 @@ if test -n $ambient_icomera_provider
 		else
 			curl -s $url | tail -c +2 | head -c -3
 		end
-	end	
+	end
 
 	echo AMBIENT_ICOMERA_PROVIDER=$ambient_icomera_provider
 	echo AMBIENT_ICOMERA_COUNTRY=$ambient_icomera_country
