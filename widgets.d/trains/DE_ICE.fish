ed52c314d7966ddb133c10ec2f5723509109e0df
embr <git@liclac.eu>
2022-10-26 16:32:04 +0200

    widgets: DE_ICE: Handle missing train numbers

--- widgets.d/trains/DE_ICE.fish
+++ widgets.d/trains/DE_ICE.fish
@@ -5,7 +5,10 @@ if [ -n "$AMBIENT_DE_ICE_STOP_NEXT_ARRIVE_DELAY" ]
 	echo "⏱ $AMBIENT_DE_ICE_STOP_NEXT_ARRIVE_DELAY"
 end
 if set -q AMBIENT_DE_ICE_TRAIN_TYPE
-	echo "🚄 $AMBIENT_DE_ICE_TRAIN_TYPE-$AMBIENT_DE_ICE_VZN"
+	echo -n "🚄 $AMBIENT_DE_ICE_TRAIN_TYPE"
+	# If iceportal.de is having a moment, we may not have the train number.
+	set -q $AMBIENT_DE_ICE_VZN; and echo -n "-$AMBIENT_DE_ICE_VZN"
+	echo
 end
 if set -q AMBIENT_DE_ICE_SPEED
 	echo "💨 $AMBIENT_DE_ICE_SPEED km/h"
